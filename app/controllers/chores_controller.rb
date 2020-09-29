1f4b5f8604b431cf07aeafcbb674479eeff1034f
Dalia <dalia.sawaya@flatironschool.com>
2020-09-28 21:50:42 -0400

    edit /chores route to clean up nested iteration

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -8,14 +8,7 @@ get '/chores' do
   else
     # we will want to grab the current_user to grab it's household which contains all of the roomates
     @roommates = current_user.household.roommates
-    #binding.pry
-    @chores = []
-    current_user.household.roommates.each do |r|
-      r.chores.each do |c|
-        @chores << c
-      end
-    end
-    @chores
+    @chores = current_user.household.roommates.map { |r| r.chores.map {|c| c} }.flatten
     erb :'chores/chores'
   end
 end
